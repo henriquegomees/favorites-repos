1d05c260455b42b2fda763e033a7f31316bb464e
Henrique Gomes <henriqueglp@gmail.com>
2018-11-12 19:09:51 -0200

    Creating components for Github and favorites repositories

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 import Header from './components/Header'
 
+import Repos from './containers/Repos'
+import Favorites from './containers/Favorites'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+
+        <div className="wrapper">
+          <Repos />
+          <Favorites />
+        </div>
       </div>
     )
   }
---
+++ src/containers/Favorites/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Favorites extends Component {
+    render() {
+        return (
+            <div className="flexed-child purple-bg">
+                
+            </div>
+        )
+    }
+}
+
+export default Favorites
