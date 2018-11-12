56f44935dcec6724afd1a6bb260a6da403f7cc73
Henrique Gomes <henriqueglp@gmail.com>
2018-11-12 18:48:21 -0200

    Initial commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+    return (
+        <header>
+            <h1>My Github Favorites</h1>
+        </header>
+    )
+}
+
+export default Header
+
