82f87d6eedae7439d738f50ca00adb60e972a892
Henrique Gomes <henriquegomeslp@yahoo.com.br>
2019-01-07 14:40:49 -0200

    Setting deploy script

---
+++ build/precache-manifest.17916628adbf8ab637274a4056d73b00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "40eaae0a64c157570601",
+    "url": "/favorites-repos/static/css/main.413374b8.chunk.css"
+  },
+  {
+    "revision": "40eaae0a64c157570601",
+    "url": "/favorites-repos/static/js/main.40eaae0a.chunk.js"
+  },
+  {
+    "revision": "4afe81845c891f3ded53",
+    "url": "/favorites-repos/static/js/1.4afe8184.chunk.js"
+  },
+  {
+    "revision": "7045bf5344449f94f52a",
+    "url": "/favorites-repos/static/js/runtime~main.7045bf53.js"
+  },
+  {
+    "revision": "63ded00268414fb070e9685a6e69617f",
+    "url": "/favorites-repos/index.html"
+  }
+];
